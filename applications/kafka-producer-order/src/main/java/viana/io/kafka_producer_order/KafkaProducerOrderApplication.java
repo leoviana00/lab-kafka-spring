3f20b80fa02ce106811cca9a9432798239378d57
Leonardo Viana <leonardoviana00@hotmail.com>
2025-08-04 09:18:03 -0300

    feat(producer): base project

---
+++ applications/kafka-producer-order/src/main/java/viana/io/kafka_producer_order/KafkaProducerOrderApplication.java
@@ -0,0 +1,13 @@
+package viana.io.kafka_producer_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerOrderApplication.class, args);
+	}
+
+}
