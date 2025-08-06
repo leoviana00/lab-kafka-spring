99d9e3c7d78f7698eab5ef0a46e052de0c869ab6
Leonardo Viana <leonardoviana00@hotmail.com>
2025-08-06 16:42:41 -0300

    feat(consumer2): base project

---
+++ kafka-consumer-order2/src/test/java/viana/io/kafka_consumer_order2/KafkaConsumerOrder2ApplicationTests.java
@@ -0,0 +1,13 @@
+package viana.io.kafka_consumer_order2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerOrder2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
